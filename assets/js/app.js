94e4d7daa59a759598601b224b30a0186e39d0b2
April Long <april11dance@gmail.com>
2019-09-16 15:59:33 -0600

    functionality, oh my!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+$(document).ready(function () { 
+
 //gloabal variables
 var counter = 20;
 var currentQuestion = 0;
@@ -73,7 +75,8 @@ var quizQuestions = [
         question: "Who is this former child star?",
         choices: ["a.Jaleel White", "b. Orlando Bloom", "c.Angus Jones", "d. Mara Wilson"],
         correctAnswer: "a.Jaleel White",
-        imageURL: "<img src= 'https://pmctvline2.files.wordpress.com/2018/08/jaleel-white.jpg?w=620'/>"
+        imageURL: "<img src= 'https://pmctvline2.files.wordpress.com/2018/08/jaleel-white.jpg?w=620'/>",
+        correctImage: 'https://pmctvline2.files.wordpress.com/2018/08/jaleel-white.jpg?w=620'
 
     },
     {
@@ -91,24 +94,30 @@ var quizQuestions = [
 
     {
         question: "Who is this former child star?",
-        choices: ["a.Jaleel White", "b. Orlando Bloom", "c.Angus Jones", "d. Mara Wilson"],
-        correctAnswer: "a.Angus Jones",
+        choices: ["a.Jonathan Nipnicki", "b. Michael Moore", "c.Macaulay Culkin", "d. Jonathan Taylor Thomas"],
+        correctAnswer: "a.Macaulay Culkin",
         imageURL: "<img src='https://timedotcom.files.wordpress.com/2019/08/macaulay-culkin-home-alone-photo.jpg?quality=85&w=594&h=450&crop=1'/>"
-    }
+    },
 
- {
+    {
         question: "Who is this former child star?",
         choices: ["a.Jonathan Lipnicki", "b. Orlando Bloom", "c.Angus Jones", "d. Jonathan Taylor Thomas"],
         correctAnswer: "a.Jonathan Lipnicki",
         imageURL: "<img src= 'https://www.thesun.co.uk/wp-content/uploads/2016/12/nintchdbpict000287158449.jpg'/>"
-    }
-    // question: "Who is this former child star?",
-    // choices:["a.Jaleel White", "b. Orlando Bloom", "c.Angus Jones", "d. Mara Wilson"],
-    // correctAnswer: "a.Jaleel White" 
+    },
+    {
+        question: "Who is this former child star?",
+        choices: ["a.Jaleel White", "b. Orlando Bloom", "c.Angus Jones", "d. Mara Wilson"],
+        correctAnswer: "c.Angus Jones",
+        imageURL: "<img src='http://d1nnx3nhddxmeh.cloudfront.net/wp-content/uploads/2017/10/26120356/TwoHalfMen27-e1509021067863.jpg'/>"
+    },
 
-    // question: "Who is this former child star?",
-    // choices:["a.Jaleel White", "b. Orlando Bloom", "c.Angus Jones", "d. Mara Wilson"],
-    // correctAnswer: "a.Jaleel White" 
+    {
+        question: "Who is this former child star?",
+        choices: ["a.Raven Symone", "b. Orlando Bloom", "c.Lark Voodhes", "d. Mara Wilson"],
+        correctAnswer: "c. Lark Voodhes",
+        imageURL: "<img src='https://gazettereview.com/wp-content/uploads/2016/02/Lark-Voorhies.jpg'/>"
+    },
 
     // question: "Who is this former child star?",
     // choices:["a.Jaleel White", "b. Orlando Bloom", "c.Angus Jones", "d. Mara Wilson"],
@@ -137,15 +146,27 @@ function nextQuestion() {
 //setting the timer to count down//
 function decrement() {
     counter--;
+    $("#time").html('The Clock is Ticking:' + counter);
     if (counter === 0) {
-        outOfTime
+        console.log("time up");
+        outOfTime();
     }
 }
 
 //When timer reaches zero//
 function outOfTime() {
     clearInterval(timer);
-    lost++;
+    losses++;
+    $("#message").html("<h2>Out of Time</h2>");
+    $("#message").append("<h3> The Correct Answer is: " +quizQuestions[currentQuestion].correctAnswer);
+    // if your answering the last question display results
+    if(currentQuestion == quizQuestions.length-1){
+        setTimeout(showResults, 2 * 1000)
+    }
+    // continue with the next question
+  else{
+        setTimeout(nextQuestion, 2 * 1000);
+    }
 }
 //
 function winnerWinner() {
@@ -161,28 +182,55 @@ function launchQuestions() {
     counter = 20;
     timer = setInterval(decrement, 1000);
 
+
     var question = quizQuestions[currentQuestion].question;
     var choices = quizQuestions[currentQuestion].choices;
     var childStars = quizQuestions[currentQuestion].imageURL;
 
     $("#time").html('Timer: ' + counter);
+    $("#photo").empty();
     $("#photo").append(childStars);
     $("#game").html('<h2>' + question + '</h2>');
-    $(launchChoices(choices)
+    for(var i=0; i<quizQuestions[currentQuestion].choices.length; i++){
+        $("#game").append("<button class='answer-button' id='button' data-name ='" +quizQuestions[currentQuestion].choices[i] +"'>" +quizQuestions[currentQuestion].choices[i] +"</button>")
+    }
+}
+$(document).on("click", ".answer-button", function(e){
+    clicked(e);
+})
+function clicked(e){
+    clearInterval(timer);
+    if($(e.target).attr("data-name") == quizQuestions[currentQuestion].correctAnswer){
+        answerCorrectly();
+    }
+    else{
+        answerIncorrectly();
+    }
+}
+function answerCorrectly(){
+    clearInterval(timer);
+    wins++;
+    $("#game").html("<h2Correct !</h2>");
+    $("#game").append("<img src='" +quizQuestions[currentQuestion].correctImage);
+    if(currentQuestion == quizQuestions.length-1){
+        setTimeout(showResults, 2 * 1000)
+    }
+    // continue with the next question
+  else{
+        setTimeout(nextQuestion, 2 * 1000);
+    }
 
-        //******How can I add image? */
-
-    );
 }
 
-//load responses to questions//
+// load responses to questions//
 // function launchChoices(choices) {
 //     var result= '';
 //      for (var i=0; i < choices.length; i++) {
-//          result += '<p class='choices "data-answer=" ${choices[i]}">${choices[i]}</p>;
+//          result += "<p class='choices' data-answer=>" ${choices[i]}">${choices[i]}</p>;
 //      }
 
 //      return result; 
 // }
 
 launchQuestions(); 
+});
